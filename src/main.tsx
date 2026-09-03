01cf4558701f824a2c16331ed0719dd68620b9da
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-09-03 16:31:24 +0200

    Keep theme menu inside app state tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import {ThemeMenu} from './components/ThemeMenu.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <ThemeMenu />
   </StrictMode>,
 );
